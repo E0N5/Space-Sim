7eb6ddaca9a82bcaa70d9050fe55a51b84f080bb
E0N5 <82764935+E0N5@users.noreply.github.com>
2023-07-30 01:59:48 +0200

    optimized the CharacterBody2D script

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,48 +1,32 @@
 extends CharacterBody2D
 
 
-var VELOCITY = 0
+var speed = 0
 
-var YAW_SPEED = 0.02
-var CUR_YAW_GRAD = 0.0
-var DES_YAW_GRAD = 0.0
+var yaw_speed = 0.02
+var cur_yaw_grad = 0.0
+var des_yaw_grad = 0.0
 var angle = 0
 
 func similar(a: float, b: float) -> bool:
-	return a >= b - 0.0001 and a <= b + 0.0001
+	return a >= b - 0.001 and a <= b + 0.001
 
 func _input(event):
 	if event.is_action_pressed("sel_pos"):
-		var DYG_Y = get_global_mouse_position().y
-		var DYG_X = get_global_mouse_position().x
-
-		DES_YAW_GRAD = snapped(atan(DYG_Y / DYG_X) if DYG_X >= 0 else atan(DYG_Y / DYG_X) + PI, 0.0001)
-		CUR_YAW_GRAD = rotation
-		angle = 0
-
-		print("x: ", DYG_X, ", y: ", DYG_Y)
-
-"""WIP
+		des_yaw_grad = get_global_mouse_position().angle()
+		print("x: ",get_global_mouse_position().x, ", y: ", get_global_mouse_position().y)
+"""wip
 	if event.is_action_pressed("thrust"):
-		VELOCITY = 6.0
-		print(VELOCITY)
-"""
-"""
-func _physics_process(delta):
-	if !similar(CUR_YAW_GRAD, DES_YAW_GRAD):
-		rotation = lerp_angle(CUR_YAW_GRAD, DES_YAW_GRAD, YAW_SPEED)
-		print("CUR: ", rotation, " DES: ", DES_YAW_GRAD)
-
-	CUR_YAW_GRAD = snapped(rotation, 0.0001)
-
-	move_and_slide()
+		velocity = 6.0
+		print(velocity)
 """
 
-func _physics_process(delta):
-	if !similar(rotation, DES_YAW_GRAD):
-		rotation = lerp_angle(CUR_YAW_GRAD, DES_YAW_GRAD, angle)
-		angle += delta
-		print("CUR: ", rotation, " DES: ", DES_YAW_GRAD)
+func _physics_process(_delta):
+	if !similar(rotation, des_yaw_grad):
+		rotation = lerp_angle(cur_yaw_grad, des_yaw_grad, 0.02)
+		cur_yaw_grad = rotation
+		
+		print("cur: ", cur_yaw_grad, " des: ", des_yaw_grad)
 
 	move_and_slide()
 
